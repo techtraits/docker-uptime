4259e412f952a061ec1a7bc3607e82e4d281e06c
Usman Ismail <usman@techtraits.com>
2015-04-21 08:49:50 -0400

    Using /etc/host specified name for linked mongodb

--- run-uptime.sh
+++ run-uptime.sh
@@ -10,7 +10,8 @@ fi
 
 
 echo "db.stats()" > testUser.js
-mongo ${MONGODB_PORT_28017_TCP_ADDR}/uptime -u root -p ${1} testUser.js ||  {
+# Note: MongoDB Container must be linked as "mongodb"
+mongo mongodb/uptime -u root -p ${1} testUser.js ||  {
 
 	echo "Password needs to be Added"
 	echo "db = db.getSiblingDB('admin')" > init-mongo.js;
@@ -18,11 +19,11 @@ mongo ${MONGODB_PORT_28017_TCP_ADDR}/uptime -u root -p ${1} testUser.js ||  {
 	echo "db = db.getSiblingDB('uptime')" >> init-mongo.js;
 	echo "db.addUser({user:'root', pwd:'${1}', roles: ['root']})" >> init-mongo.js;
 
-	mongo ${MONGODB_PORT_28017_TCP_ADDR} init-mongo.js;
+	mongo mongodb init-mongo.js;
 };
 
-sed "s/MONGO_SERVER/${MONGODB_PORT_27017_TCP_ADDR}/g" config/config_template.yaml | sed "s/MONGO_PASSWORD/${1}/g" > config/default.yaml
-
+sed "s/MONGO_PASSWORD/${1}/g" > config/default.yaml
+-
 
 # Run custom initilization script if it exists
 if [ -f /tmp/uptime/init-uptime.sh ];
